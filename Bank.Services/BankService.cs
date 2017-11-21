05e7b4f7cfaa05ddb73471de6916a51f161eafd8
Jay Jackson <jaynickjackson@gmail.com>
2017-11-21 00:20:04 -0500

    began bank.services

---
+++ Bank.Services/BankService.cs
@@ -0,0 +1,37 @@
+﻿using Bank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Services
+{
+    class BankService
+    {
+        public BankService()
+        {
+
+        }
+
+        public IEnumerable<Customers> GetCustomers()
+        {
+            using (var ctx = new BankDBEntities())
+            {
+                return ctx.Customers;
+            }
+        }
+
+        public Customers CreateCustomer()
+        {
+            using (var ctx = new BankDBEntities())
+            {
+                var entity = 
+                    new Customers
+                    {
+
+                    }
+            }
+        }
+    }
+}
