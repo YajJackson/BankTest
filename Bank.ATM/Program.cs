5f266567a74d4587ba4f3260dfd10e0b0c2185e1
Jay Jackson <jaynickjackson@gmail.com>
2017-11-22 10:31:53 -0500

    added ascii art landing, and scaffold User class

--- Bank.ATM/Program.cs
+++ Bank.ATM/Program.cs
@@ -10,6 +10,27 @@ namespace Bank.ATM
     {
         static void Main(string[] args)
         {
+            Console.Title = "Bank ATM";
+            string bankImage = @"
+                            ..]OOoo\`.                   
+                        ..]OO/`*****[OoO].               
+                    .,]OO/`,]]*]*],]]]]*[OoO].           
+                .,/OO/`*****@@=\^=\^=\`*****,\oO]..      
+              =Oo`ooOOOOOOOOOOOOOOOOOOOOOOOOOOoo^,\O     
+                =[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[`       
+                 =ooooO@@@Ooooo@@@@oOoOo@@@@ooooO        
+                  .,O@@@@@@.\O@@@@@^=O@@@@@@^,O@         
+                  .*O@@@@@@.=O@@@@@^=O@@@@@@^*O@         
+                  .*O@@@@@@.=O@@@@@^=O@@@@@@^*O@         
+                  .*O@@@@@@.=O@@@@@^=O@@@@@@^*O@         
+                  .*O@@@@@@.=O@@@@@^=O@@@@@@^*O@         
+                  .*O@@@@@@./O@@@@@^=O@@@@@@^*O@         
+                  *oO@@@@@^*oO@@@@@^=OO@@@@@*=O@^        
+                 ********************************.       
+               .[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[..     
+              ......,[[[[[[[[[[[[[[[[[[[[[[[[`*.....     
+            ";
+            Console.WriteLine(bankImage);
             Console.WriteLine("Welcome to the bank.\n"+
                               "New User: 0\n"+
                               "Login: 1");
@@ -28,7 +49,6 @@ namespace Bank.ATM
                 default:
                     break;
             }
-
         }
     }
 }
---
+++ Bank.ATM/User.cs
@@ -0,0 +1,17 @@
+﻿using Bank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.ATM
+{
+    class User
+    {
+        public int UserID { get; set; }
+        public bool LoggedIn { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Accounts> Accounts { get; set; }
+    }
+}
